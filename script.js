2ab3d782f7798e9f42263741e1f17fb7bff25c31
Thomas Salmann <thomas.salmann@gmail.com>
2022-12-08 16:35:05 +0100

    add: adds counter function

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-const
+const main = document.querySelector("main");
+const number = document.querySelector(".counter");
+const reset = document.querySelector(".reset");
+const root = document.querySelector(":root");
+let counter = 0;
+let barCounter = 0;
+
+function counting() {
+  counter++;
+  barCounter++;
+  if (barCounter === 100) {
+    barCounter = 0;
+  }
+  console.log(counter);
+  number.innerText = counter;
+  root.style.setProperty("--gold", barCounter + "%");
+  root.style.setProperty("--white", "0%");
+}
+
+main.addEventListener("click", counting);
+
+reset.addEventListener("click", function (r) {
+  number.textContent = "0";
+  counter = 0;
+  barCounter = 0;
+  root.style.setProperty("--gold", "0%");
+  reset.blur();
+});
+
+document.addEventListener("keydown", function (e) {
+  if (["Enter", " "].includes(e.key)) {
+    counting();
+  }
+});
